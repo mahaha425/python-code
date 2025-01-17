4068736fda792231c0ed32770f6cd42ba73b50e8
mahaha425 <jp2024213768@qmul.ac.uk>
2025-01-17 09:07:42 +0800

    exercise

---
+++ 1.16.py
@@ -0,0 +1,54 @@
+import numpy as np
+# arr = np.array([-1,2,3,4,5])
+# print(abs(arr))
+# arr = np.array([3 + 4j,2,3,4,5])
+# print(abs(arr))
+
+# arr_angle = np.linspace(0,np.pi,3)
+# print(arr_angle)
+# print(np.sin(arr_angle))
+# print(np.cos(arr_angle))
+# print(np.tan(arr_angle))
+
+# arr = np.array([-1,0,1])
+# print(np.arcsin(arr))
+# print(np.arccos(arr))
+# print(np.arctan(arr))
+
+# arr = np.array([1,2,3])
+# print(np.exp(arr))  #计算e的n次方
+# print(np.exp2(arr)) #计算2的n次方
+# print(np.power(3,arr)) #计算3的n次方
+# print(np.log(arr))  #计算以e为底数的对数
+# print(np.log2(arr)) #计算以2为底数的对数
+# print(np.log10(arr)) #计算以10为底数的对数
+
+# arr_1 = np.linspace(0,9,10,dtype = int)
+# print(arr_1)
+# arr_2 = np.empty(10)
+# print(arr_2)
+# np.multiply(arr_1,10,out = arr_2)
+# print(arr_2)
+
+# arr_1 = np.linspace(0,5,5,dtype = int)
+# arr = np.zeros(10)
+# np.power(2,arr_1,out = arr[::2])
+# print(arr)
+
+# arr_1 = np.linspace(0,5,6,dtype = int)
+# print(arr_1)
+# print(np.add.reduce(arr_1))
+# print(np.add.accumulate(arr_1))
+
+# arr_1 = np.linspace(1,5,5,dtype = int)
+# print(arr_1)
+# print(np.multiply.outer(arr_1,arr_1))
+
+# arr_1 = np.arange(0,12)
+# arr_2 = arr_1.reshape((3,4))
+# print(arr_2)
+# print(np.sum(arr_2))
+# print(np.sum(arr_2,axis = 0)) #折叠第一个维度，即每一列的值都将被聚合。
+# print(np.sum(arr_2,axis = 1)) #折叠第二个维度，即每一行的值都将被聚合。
+
+
