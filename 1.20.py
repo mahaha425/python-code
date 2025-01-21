f3831011190f23ca65f12f78b63d75457796d710
mahaha425 <jp2024213768@qmul.ac.uk>
2025-01-21 10:22:35 +0800

    exercise

--- 1.20.py
+++ 1.20.py
@@ -24,10 +24,10 @@
 # print ('对换数组：')
 # print (np.transpose(a))
 
-import numpy as np
-a = np.arange(12).reshape(3,4)
-print ('原数组：')
-print (a)
-print ('\n')
-print ('转置数组：')
-print (a.T)
+# import numpy as np
+# a = np.arange(12).reshape(3,4)
+# print ('原数组：')
+# print (a)
+# print ('\n')
+# print ('转置数组：')
+# print (a.T)
